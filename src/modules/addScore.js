21052ac671ad10ddda7c9fb048bf1d38a0eac60b
Kasaija Kenneth <kasaijak@gmail.com>
2022-10-19 04:14:59 +0300

    Run build npm

---
+++ src/modules/addScore.js
@@ -0,0 +1,14 @@
+
+const name =document.getElementById('name');
+const score =document.getElementById('score');
+const list = document.querySelector('.list');
+
+const addScore = () => {
+  if((name.value.length >=1) && (score.value.length >=1)) {
+    const newList =document.createElement('li');
+    newList.classList.add('listItem');
+    newList.innerText = `${name.value}: ${score.value}`;
+    list.appendChild(newList);
+  }
+}
+module.exports = addScore;
