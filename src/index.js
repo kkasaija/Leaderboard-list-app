f85a82a30d8ca23e9dc709d61422bc44302811aa
Kasaija Kenneth <kasaijak@gmail.com>
2022-10-19 04:10:11 +0300

    Add src/index.html structure

--- src/index.js
+++ src/index.js
@@ -1,2 +1,8 @@
 import _ from 'lodash';
 import './style.css';
+
+import {addScore} from './modules/addScore.js';
+const submit =document.getElementById('submit');
+
+
+submit.addEventListener('click', addScore);
